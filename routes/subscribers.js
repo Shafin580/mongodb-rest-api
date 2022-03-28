88c6f79934815bfdc12f4f10815496673717950d
Shafin580 <shafinwork580@gmail.com>
2022-03-28 23:45:25 +0600

    get by id route

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -12,12 +12,40 @@ router.get("/", async (req, res) => {
   }
 });
 //Getting One
-router.get("/:id", (req, res) => {});
+router.get("/:id", getSubscriber, (req, res) => {
+  res.send(res.subscriber);
+});
 //Creating One
-router.post("/", (req, res) => {});
+router.post("/", async (req, res) => {
+  const subscriber = new Subscriber({
+    name: req.body.name,
+    subscriberToChannel: req.body.subscriberToChannel,
+  });
+  try {
+    const newSubscriber = await subscriber.save();
+    res.status(201).json(newSubscriber);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
 //Updating One
-router.patch("/:id", (req, res) => {});
+router.patch("/:id", getSubscriber, (req, res) => {});
 //Deleting One
-router.delete("/:id", (req, res) => {});
+router.delete("/:id", getSubscriber, (req, res) => {});
+
+async function getSubscriber(req, res, next){
+    let subscriber;
+    try{
+        subscriber = await Subscriber.findById(req.params.id);
+        if(subscriber == null){
+            return res.status(404).json({ message: "Can not find subscriber" });
+        }
+    }catch(error){
+        return res.status(500).json({ message: error.message });
+    }
+
+    res.subscriber = subscriber;
+    next();
+}
 
 module.exports = router;
