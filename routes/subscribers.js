7ae2b3f986225f655c3194fbfd872b008d2e673c
Shafin580 <shafinwork580@gmail.com>
2022-03-28 22:07:41 +0600

    Creating Model

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -1,10 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const Subscriber = require('../models/subscriber');
 
 //Getting All
+router.get('/', (req, res) => {
+    res.send("Hello, World!");
+});
 //Getting One
+router.get('/:id', (req, res) => {
+
+});
 //Creating One
+router.post('/', (req, res) => {
+
+});
 //Updating One
+router.patch('/:id', (req, res) => {
+
+});
 //Deleting One
+router.delete('/:id', (req, res) => {
+
+});
 
 module.exports = router;
